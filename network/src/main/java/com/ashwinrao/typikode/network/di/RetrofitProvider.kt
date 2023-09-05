579409642e12bc703cd9ae33ee9d3344c22f2aa2
Ashwin Rao <ashwinravrao@gmail.com>
2023-09-04 19:34:42 -0500

    Add retrofit provider to :network

---
+++ network/src/main/java/com/ashwinrao/typikode/network/di/RetrofitProvider.kt
@@ -0,0 +1,30 @@
+package com.ashwinrao.typikode.network.di
+
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RetrofitProvider {
+
+    private val typicodeBaseUrl = "https://jsonplaceholder.typicode.com/"
+
+    @Provides
+    @Singleton
+    fun provideRetrofitClient(): Retrofit =
+        Retrofit.Builder()
+            .baseUrl(typicodeBaseUrl)
+            .addConverterFactory(
+                Json.asConverterFactory(
+                    contentType = "application/json".toMediaType()
+                )
+            ).build()
+
+}
